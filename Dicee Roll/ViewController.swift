b1c2e888dda937beabcf13977eed5eafeb9ea6c4
JonasRosendo <jonasrosendo@live.com>
2019-09-11 19:37:27 -0300

    Initial Commit

---
+++ Dicee Roll/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Dicee Roll
+//
+//  Created by Jonas Rosendo on 23/08/2019.
+//  Copyright © 2019 Jonas Rosendo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        print(randomDiceIndex1)
+        
+        diceImage1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImage2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+    }
+    
+}
+
